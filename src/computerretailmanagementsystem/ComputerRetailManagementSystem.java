ab121c5c6e464557c733bd49d0c2e956f930d4fd
foodycookie <onghao.howard@gmail.com>
2025-03-19 00:22:59 +0800

    hi

---
+++ src/computerretailmanagementsystem/ComputerRetailManagementSystem.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package computerretailmanagementsystem;
+
+/**
+ *
+ * @author Ong Hao Howard
+ */
+public class ComputerRetailManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
