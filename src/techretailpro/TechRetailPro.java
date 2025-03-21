f7270ecb5979cc406c5380e64567ac195ff7cdea
foodycookie <onghao.howard@gmail.com>
2025-03-21 13:35:32 +0800

    renamed

--- src/techretailpro/TechRetailPro.java
+++ src/techretailpro/TechRetailPro.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package computerretailmanagementsystem;
+package techretailpro;
 
 /**
  *
  * @author Ong Hao Howard
  */
-public class ComputerRetailManagementSystem {
+public class TechRetailPro {
 
     /**
      * @param args the command line arguments
